a8785066de035d8f8aebcf9bc3afe0a743de5241
ChaDomi <klanzbat@gmail.com>
2021-02-09 17:38:28 +0800

    ddwadwadaad

--- database.js
+++ database.js
@@ -201,7 +201,7 @@ function getQueue(company_id) {
         .query(sql, sqlParams)
         .then((result) => {
             console.log(result);
-            return (result.rows.queue_id,result.rows.status);
+            return result.rows.queue_id;
         })
         .catch((error) => {
             client.end();
